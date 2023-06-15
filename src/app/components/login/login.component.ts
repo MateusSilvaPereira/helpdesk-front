14213be25fed93ec29692ccbd03d8784f9e1399d
Mateus Silva Pereira <mateussilvapereira2018@gmail.com>
2023-06-14 20:46:25 -0400

    Serviço de mensagem implementado

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credenciais';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -17,9 +17,14 @@ export class LoginComponent implements OnInit {
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
 
-  constructor() { }
+  constructor(private toastr: ToastrService) { }
 
   ngOnInit(): void {
+  }
+
+  logar(){
+    this.toastr.error('Usuário e/ou senha inválidos!','Login');
+    this.creds.senha = "";
   }
 
   validaCampos(): boolean {
